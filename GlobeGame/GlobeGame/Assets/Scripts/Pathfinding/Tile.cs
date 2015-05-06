fe155a336a6033903c403ccc673d1a0e30558537
Nachtrind <Kerstin@Nachtrind.de>
2015-05-06 19:18:55 +0200

    Shit

---
+++ GlobeGame/GlobeGame/Assets/Scripts/Pathfinding/Tile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile
+{
+
+	bool walkable;
+	float size;
+	float posX;
+	float posY;
+
+	// 1  2  3
+	// 4  5  6
+	// 7  8  9
+	//Each node can have up to 8 neighbours, 5 is self
+	Tile[] neighbours = new Tile[9];
+
+	public Tile (float _x, float _y, float _size, bool _walkable)
+	{
+
+		this.posX = _x;
+		this.posY = _y;
+		this.size = _size;
+		this.walkable = _walkable;
+
+	}
+
+}
---
+++ GlobeGame/GlobeGame/Assets/Scripts/Pathfinding/World.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class World
+{
+
+	public List<GameObject> gridStartingPoints;
+
+	// Use this for initialization
+	public void GenerateWorld (float _tileSize)
+	{
+		foreach (GameObject obj in gridStartingPoints) {
+			GenerateGraph (obj, _tileSize);
+		}
+	}
+
+	private void GenerateGraph (GameObject _startingPoint, float _size)
+	{
+
+	}
+
+}
