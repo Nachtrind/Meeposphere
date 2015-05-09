87648c11f2feddf160b70c4bcec539e7c9383b9d
Nachtrind <Kerstin@Nachtrind.de>
2015-05-09 11:21:42 +0200

    Meeple Movement Fix

--- GlobeGame/GlobeGame/Assets/Scripts/Gravity&Movement/MeepleController.cs
+++ GlobeGame/GlobeGame/Assets/Scripts/Gravity&Movement/MeepleController.cs
@@ -4,13 +4,17 @@ using System.Collections;
 public class MeepleController : MonoBehaviour
 {
 	public GlobeGravity gravity;
-
 	public Rigidbody rigid;
 	Vector3 target;
 	Transform mTrans;
 	float acceptableDistance = 0.2f;
-	float speed = 4.5f;
+	float speed = 6.5f;
+	Vector3 pushback;
+
 	bool gotCalledByPlayer;
+	bool hitObstacle;
+	float obstacleTimer = 0.1f;
+
 	Camera arCam;
 	Quaternion targetRot;
 
@@ -18,43 +22,34 @@ public class MeepleController : MonoBehaviour
 	void Start ()
 	{
 		mTrans = transform;
-		rigid = GetComponent<Rigidbody> ();
 		target = mTrans.position;
 
-
+		rigid = GetComponent<Rigidbody> ();
 		rigid.constraints = RigidbodyConstraints.FreezeRotation;
 		rigid.useGravity = false;
-
-
 
 
 		//acceptable Distance is Meeple width
 		acceptableDistance = GetComponent<MeshFilter> ().mesh.bounds.size.x * mTrans.localScale.x * 3;
 		gotCalledByPlayer = false;
+
+		//Set Main Camera
 		Camera[] cams = Camera.allCameras;
 		foreach (Camera cam in cams) {
 			if (cam.tag.Equals ("MainCamera")) {
 				this.arCam = cam;
 			}
 		}
-		Debug.Log ("CAMERA: " + this.arCam);
 	}
 
 	void Update ()
 	{
-
-		//Debug: Show forward direction
-		Vector3 forward = transform.TransformDirection (Vector3.forward) * 5;
-		Debug.DrawRay (transform.position, forward, Color.blue);
-
-
 		if (Input.GetMouseButtonDown (0)) {
 
 			gotCalledByPlayer = true;
 
 			Ray ray = this.arCam.ScreenPointToRay (Input.mousePosition);
 			RaycastHit hit;
-		
 			if (Physics.Raycast (ray, out hit, 100)) {
 				target = hit.point;
 				mTrans.rotation = Quaternion.LookRotation (target, mTrans.up);
@@ -65,19 +60,39 @@ public class MeepleController : MonoBehaviour
 
 	// Update is called once per frame
 	void FixedUpdate ()
-	{		
+	{
+
+
 		float currentDistance = Vector3.Distance (mTrans.position, target);
-
-		Debug.Log ("target: " + target + "/Current Position" + mTrans.position);
-		Debug.Log ("CurrentDistance: " + currentDistance + "/" + acceptableDistance);
-
 
 		if (currentDistance > acceptableDistance) {
 			rigid.MovePosition (mTrans.position + mTrans.forward * speed * Time.deltaTime);
+
+			//Check in Front
+			Ray ray = new Ray (mTrans.position, mTrans.forward);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit, 1)) {
+				hitObstacle = true;
+				pushback = mTrans.forward.normalized * -50;
+				Debug.Log ("hitObstacle");
+				target = transform.position;
+				gotCalledByPlayer = false;
+			}
 		}
 
+		if (hitObstacle && obstacleTimer > 0) {
+
+			rigid.AddForce (pushback);
+			obstacleTimer -= Time.deltaTime;
+		} else if (hitObstacle && obstacleTimer <= 0) {
+			hitObstacle = false;
+			obstacleTimer = 0.1f;
+		}
 
 		gravity.Gravitate (mTrans);
+
+
 	}
 
 
