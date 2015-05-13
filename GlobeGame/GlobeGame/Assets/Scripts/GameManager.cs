4ca770a836d37015ed128da19eebf6d0377b96ec
Sandra Schmidt <s0543739@htw-berlin.de>
2015-05-13 14:17:35 +0200

    level 2/3

--- GlobeGame/GlobeGame/Assets/Scripts/GameManager.cs
+++ GlobeGame/GlobeGame/Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour {
 
 	private static GameManager instance;
-	public List<
+//	public List<
 	
 	public static GameManager Instance {
 		get { return instance ?? (instance = new GameObject ("GameManager").AddComponent<GameManager> ()); }
