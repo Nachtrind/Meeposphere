75d3ebd4a2cd80a7309a4738326a16b16e8be388
Nachtrind <Kerstin@Nachtrind.de>
2015-05-20 10:53:41 +0200

    Felix Fix2

--- GlobeGame/GlobeGame/Assets/Scripts/Utilities.cs
+++ GlobeGame/GlobeGame/Assets/Scripts/Utilities.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
-using System.IO;
 
 public class Utilities
 {
 
-	public LevelGraph LoadXMLFile (string _path)
+		public LevelGraph LoadXMLFile (string _path)
 	{  
 		/*
 		var serializer = new XmlSerializer (typeof(LevelGraph));
@@ -17,18 +17,28 @@ public class Utilities
 		using (var stream = new FileStream(_path, FileMode.Open)) {
 			return serializer.Deserialize (stream) as LevelGraph;
 		}*/
-
+		
 		TextAsset _xml = new TextAsset();
 		_xml = (TextAsset)Resources.Load("Level2X-8", typeof(TextAsset));
 		XmlDocument xmldoc = new XmlDocument ();
 		xmldoc.LoadXml (_xml.text);
-
-
+		
+		
 		XmlSerializer serializer = new XmlSerializer(typeof(LevelGraph));
 		XmlReader reader = XmlReader.Create (new StringReader (_xml.text));
 		LevelGraph lGraph = (LevelGraph) serializer.Deserialize (reader);
-
+		
 		return lGraph;
+	}
+			
+			public void SaveLevelGraph (LevelGraph _graph, string _path)
+	{
+		
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (_path);
+		bf.Serialize (file, _graph);
+		Debug.Log ("Saved Graph at: " + _path);
+		file.Close ();
 	}
 
 	public void SaveXMLFile(string _path, LevelGraph _graph)
@@ -39,15 +49,6 @@ public class Utilities
 		}
 	}
 	
-	public void SaveLevelGraph (LevelGraph _graph, string _path)
-	{
-		BinaryFormatter bf = new BinaryFormatter ();
-		FileStream file = File.Create (_path);
-		bf.Serialize (file, _graph);
-		Debug.Log ("Saved Graph at: " + _path);
-		file.Close ();
-	}
-
 	/*
 	private SavableGraph CreateSavableGraph (LevelGraph _graph)
 	{
@@ -81,28 +82,28 @@ public class Utilities
 
 	}
 */
-
-
-
+	
+	
+	
 	public LevelGraph LoadLevelGraph (GameObject _globe, string _path)
 	{
-
+		
 		LevelGraph returnGraph = new LevelGraph ();
-
+		
 		if (File.Exists (_path)) {
 			BinaryFormatter bf = new BinaryFormatter ();
 			FileStream file = File.Open (_path, FileMode.Open);
 			returnGraph = (LevelGraph)bf.Deserialize (file);
 			file.Close ();
 		} else {
-			Debug.LogError ("PATH DOESN'T EXIST");
+			Debug.LogError("PATH DOESN'T EXIST");
 		}
-
-//		returnGraph = Resources.LoadAssetAtPath (_path, LevelGraph);
-
+		
+		//		returnGraph = Resources.LoadAssetAtPath (_path,typeof(LevelGraph)) as LevelGraph;
+		
 		return returnGraph;
 	}
-
+	
 	/*
 	private LevelGraph CreateTileGraph (SavableGraph _graph, GameObject _globe)
 	{
@@ -136,31 +137,31 @@ public class Utilities
 		return returnGraph;
 
 	}*/
-
+	
 	public Vector3 NearestVertexTo (Vector3 _point, GameObject _obj)
 	{ 
-
+		
 		Vector3 locPoint = _obj.transform.InverseTransformPoint (_point);
-
+		
 		Mesh mesh = _obj.GetComponent<MeshFilter> ().mesh;
 		float minDistance = Mathf.Infinity;
 		Vector3 nearestVertex = Vector3.zero;
-	
+		
 		// scan all vertices to find nearest
 		foreach (Vector3 vertex in mesh.vertices) {
 			Vector3 distanceVec = locPoint - vertex;
 			float distance = distanceVec.sqrMagnitude;
-		
+			
 			if (distance < minDistance) {
 				minDistance = distance;
 				nearestVertex = vertex;
 			}
 		}
-	
+		
 		// convert nearest vertex back to world space
 		return _obj.transform.TransformPoint (nearestVertex);
 	}
-
+	
 	//returns the index of the Tile that has been clicked
 	public Tile GetClickedTile (Vector3 _clickHit, List<Tile> _tiles, GameObject _world)
 	{
@@ -170,25 +171,25 @@ public class Utilities
 				return t;
 			}
 		}
-
+		
 		return null;
-
+		
 	}
-
-
+	
+	
 	//rotates an object on the surface of the globe
 	//TODO: code cleanup
 	public void RotateOnGlobeSurface (GameObject _obj, GameObject _globe)
 	{
-
+		
 		Vector3 gravityUp = (_obj.transform.position - _globe.transform.position).normalized;
 		Vector3 meepleUp = _obj.transform.up;
-
+		
 		Quaternion targetRot = Quaternion.FromToRotation (meepleUp, gravityUp) * _obj.transform.rotation;
 		_obj.transform.rotation = Quaternion.Slerp (_obj.transform.rotation, targetRot, 50 * Time.deltaTime);
-
-
+		
+		
 	}
-
-
+	
+	
 }
