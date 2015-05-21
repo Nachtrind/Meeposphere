f75ecd2119cbab11cf68a67588685a05fad17f3d
FelixOes <s0543732@htw-berlin.de>
2015-05-21 13:05:15 +0200

    hurricanes

--- GlobeGame/GlobeGame/Assets/hurricane_move.cs
+++ GlobeGame/GlobeGame/Assets/hurricane_move.cs
@@ -29,8 +29,8 @@ public class hurricane_move : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Mathf.PingPong(Time.time,move_time) < move_time/2){
-			GetComponent<Rigidbody>().velocity = direction *speed;}
-		else {GetComponent<Rigidbody>().velocity = backdirection *speed;}
+			GetComponent<Rigidbody>().velocity = direction.normalized *speed;}
+		else {GetComponent<Rigidbody>().velocity = backdirection.normalized *speed;}
 
 		 //GetComponent<BoxCollider>().
 	
