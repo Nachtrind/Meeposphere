03c3e721c8870c53014884a6b4ad3e2421d75dc0
FelixOes <s0543732@htw-berlin.de>
2015-05-21 12:18:37 +0200

    Hurricane_move

--- GlobeGame/GlobeGame/Assets/hurricane_move.cs
+++ GlobeGame/GlobeGame/Assets/hurricane_move.cs
@@ -6,15 +6,39 @@ public class hurricane_move : MonoBehaviour {
 	public Vector3 originalposition;
 	public float move_time;
 	public float speed;
+	public GameObject target;
+	public GameObject start;
+
+	public Vector3 targetposition;
+	public Vector3 direction;
+	public Vector3 backdirection;
+
 
 	// Use this for initialization
 	void Start () {
 
-		GetComponent().transform.position = Vector3 
-	}
+		originalposition = start.GetComponent<Transform>().position;
+		targetposition = target.GetComponent<Transform>().position;
+		direction = targetposition - originalposition;
+		backdirection = originalposition - targetposition;
 	
+	}
+	//private void Normalize(direction);
+	//{}
+
 	// Update is called once per frame
 	void Update () {
+		if(Mathf.PingPong(Time.time,move_time) < move_time/2){
+			GetComponent<Rigidbody>().velocity = direction *speed;}
+		else {GetComponent<Rigidbody>().velocity = backdirection *speed;}
+
+		 //GetComponent<BoxCollider>().
 	
 	}
+	void OnCollisionEnter(Collision collision) {
+		if (collision.collider.tag.Equals("Meeple")) {
+			GetComponent<MeepleController>().HitByHurricane();
+
+		}
+		}
 }
