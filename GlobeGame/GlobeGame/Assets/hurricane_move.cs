80aad0d7faf81a6eb66a087c9f7d9d30000b7dd7
FelixOes <s0543732@htw-berlin.de>
2015-05-21 10:26:50 +0200

    grfsdgfsg

---
+++ GlobeGame/GlobeGame/Assets/hurricane_move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hurricane_move : MonoBehaviour {
+
+	public Vector3 originalposition;
+	public float move_time;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent().transform.position = Vector3 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
