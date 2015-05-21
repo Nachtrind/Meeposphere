329f54cb29064d9fddeb46473b61011a339db490
Sandra Schmidt <s0543739@htw-berlin.de>
2015-05-21 13:11:05 +0200

    lava drop

---
+++ GlobeGame/GlobeGame/Assets/Scripts/lavaDrop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lavaDrop : MonoBehaviour {
+
+	public Vector3 originalposition;
+
+	// Use this for initialization
+	void Start () {
+		
+    	originalposition = this.GetComponent<Transform>().transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.collider.tag.Equals ("Meeple")) {
+			GetComponent<MeepleController> ().HitByLava ();
+			} if (collision.collider.tag.Equals ("Globe")) {
+				originalposition =GetComponent<Transform>().transform.position ;
+		}
+	}
+}
